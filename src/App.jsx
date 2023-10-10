9022c91663175ed393c759eed271da4e62228c22
Anantharajupeta gadidala <sivajigsivajig703@gmail.com>
2023-10-10 15:40:17 +0000

    fixing screen height issue in mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
    <>
    <Navbar/>
    <Home/>
+   
    <About/>
    <Skills/>
    <Workk/>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ import mongodb from "../assets/mongodb.png"
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+<div name='skills' className='w-full min-h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
